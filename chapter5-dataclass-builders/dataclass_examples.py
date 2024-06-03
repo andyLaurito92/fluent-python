c914170daf362a8e341ffb7efc84551292957430
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-06-03 09:34:18 +0200

    Example on when a dataclass is hashable

--- chapter5-dataclass-builders/dataclass_examples.py
+++ chapter5-dataclass-builders/dataclass_examples.py
@@ -22,3 +22,47 @@ where the attribute was returned as a descriptor.
 """
 
 print(Person.age)
+
+"""
+From the dataclass annotation, the parameters that are passed are:
+- frozen: Makes the class immutable. If you try to change an attribute, it will raise an exception.
+- eq: If True, the class implements __eq__() method. Default is True.
+- order: If True, the class implements __lt__(), __le__(), __gt__(), and __ge__() methods. Default is False.
+- init: If True, the class implements a __init__() method that initializes the attributes. Default is True.
+- repr: If True, the class implements a __repr__() method. Default is True.
+- unsafe_hash: If True, the class implements a __hash__() method. Default is False.
+
+Frozen and order are the more likely to be changed.
+"""
+
+
+"""
+Hashing dataclasses
+
+Note the difference between the following two classes:
+"""
+
+@dataclass
+class UnhashableCity:
+    name: str
+    population: int
+    country: str
+    coordinate: tuple
+
+@dataclass(frozen=True)
+class City:
+    name: str
+    population: int
+    country: str
+    coordinate: tuple
+
+
+buenosaires_hashable = City('Buenos Aires', 2891000, 'AR', (-34.61, -58.38))
+buenosaires_unhashable = UnhashableCity('Buenos Aires', 2891000, 'AR', (-34.61, -58.38))
+
+print(f"Hash of buenos aires: {hash(buenosaires_hashable)}")
+
+try: 
+    print(hash(buenosaires_unhashable))
+except Exception as e:
+    print("When trying to hash a dataclass which can be mutable (frozen = False), then an exception is thrown ", e)
