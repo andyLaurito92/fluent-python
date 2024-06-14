e18fdf20b8e2eb7fd93cca656d884f6a51fae1d4
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-06-14 09:30:12 +0200

    Adding default values + weak reference examples

--- chapter6-object-references-aliases/aliases_example.py
+++ chapter6-object-references-aliases/aliases_example.py
@@ -60,3 +60,45 @@ Why would we want something like the above? Because if the elements are immutabl
 is way faster than having to copy each element. Think that there can be some nested data
 structures which make the copy really expensive in time
 """
+
+"""
+When working with function parameters, remember that parameters are passed by object reference, meaning that parameters are aliases
+"""
+
+def f(a, b):
+    a += b
+    return a
+
+x = 1
+y = 2
+print(f"Result of f(x, y): {f(x, y)}")
+print(f"x: {x}, y: {y}")
+
+a = [1, 2]
+b = [3, 4]
+print(f"Result of f(a, b): {f(a, b)}")
+print(f"a: {a}, b: {b}")
+
+t1 = (10, 20)
+t2 = (30, 40)
+print(f"Result of f(t1, t2): {f(t1, t2)}")
+print(f"t1: {t1}, t2: {t2}")
+
+"""
+Avoid using mutable objects as default parameters in functions
+"""
+
+def f(a, data=[]):
+    data.append(a)
+    return data
+
+print(f"Result of f(1): {f(1)}")
+print(f"Result of f(2): {f(2)}")
+print(f"Result of f(3): {f(3)}")
+print(f"Result of f(4): {f(4, [1])}")
+
+"""
+Where does data live? In the frame that loads the function. The default value is created when the function is imported. Every time a function is called, then a frame is created and the parameters of the function are binded in the frame. For data, this means that either we bind the reference of the object passed by parameter or the list created when the module was defined, which lives again in the frame of the module.
+
+See this stackoverflow for more info on this: https://stackoverflow.com/questions/38937721/global-frame-vs-stack-frame
+"""
