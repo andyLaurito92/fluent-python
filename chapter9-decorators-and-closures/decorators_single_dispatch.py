6fc526ed6e050563505b0cfac390c85d57f718a1
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-07-08 17:01:52 +0200

    Adding html.register for more types

--- chapter9-decorators-and-closures/decorators_single_dispatch.py
+++ chapter9-decorators-and-closures/decorators_single_dispatch.py
@@ -8,6 +8,9 @@ this, we will use the single dispatch instead of doing a lot of if/elif/else
 """
 
 from functools import singledispatch
+from collections import abc
+import decimal
+from fractions import Fraction
 
 
 @singledispatch
@@ -19,3 +22,23 @@ def htmlize(obj: object) -> str:
 def _(astr: str) -> str:
     newstr = astr.replace('\n', '<br/>')
     return f"<p>{newstr}</p>"
+
+@htmlize.register
+def _(anint: int) -> str:
+    return f"<pre>{anint} ({hex(anint)})</pre>"
+
+@htmlize.register
+def _(abool: bool) -> str:
+    return f"<pre>{abool}</pre>"
+
+@htmlize.register(decimal.Decimal)
+@htmlize.register
+def _(afloat: float) -> str:
+    frac = Fraction(afloat).limit_denominator()
+    return f"<pre>{afloat} ({frac.numerator}/{frac.denominator})</pre>"
+
+@htmlize.register
+def _(seq: abc.Sequence) -> str:
+    html_elements = ''.join([f"<li>{htmlize(elem)}</li>"
+                             for elem in seq])
+    return f"<ul>{html_elements}</ul>"
