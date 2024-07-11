93bef661aa6e0d085a60b8151d1272922a742d1f
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-07-11 09:28:22 +0200

    Adding example on why python is lexical scoped

--- chapter9-decorators-and-closures/scope_rules.py
+++ chapter9-decorators-and-closures/scope_rules.py
@@ -44,3 +44,48 @@ from dis import dis
 dis(myfun1)
 dis(myfun2)
 dis(myfun3)
+
+
+"""
+Example on why python is not dynamically scoped
+"""
+
+print("We define a function getx with a free variable x")
+def getx():
+    return x
+
+
+x = 8
+
+"""
+Because python is lexical scoped, x is always defined according to
+where the function getx was defined. This means that x will always
+be attached to the global variable x
+"""
+
+def myconfusingfunction():
+    x = 4
+    y = 5
+    z = getx()
+    print(f"x: {x}, y: {y}, z:{z}")
+
+print("What wil this function return?")
+myconfusingfunction()
+
+mystr = (
+    "Why?, because python is lexical scoped, meaning that even though "
+    "we defined x inside myconfusingfunction, python always binds "
+    "variables to values by where the function was defined, not where "
+    "is being executed")
+
+x = 1
+myconfusingfunction()
+
+
+"""
+What would a dynamic scoped language do? Bind x dynamically according to
+the execution of the program
+
+Example: elisp :).
+See more here: https://www.gnu.org/software/emacs/manual/html_node/elisp/Dynamic-Binding.html
+"""
