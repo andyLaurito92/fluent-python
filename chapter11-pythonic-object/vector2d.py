fd403ea119c60783b7d45d47f370ef91ce012f68
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-07-17 10:24:27 +0200

    Adding special methods for vector2d

--- chapter11-pythonic-object/vector2d.py
+++ chapter11-pythonic-object/vector2d.py
@@ -1,3 +1,6 @@
+from math import sqrt
+from typing import Iterable
+
 class Vector2D:
     def __init__(self, x: float, y: float) -> None:
         self.x = x
@@ -6,3 +9,13 @@ class Vector2D:
     def __repr__(self) -> str:
         """ Returns programmer representation of vector 2d """
         return f"Vector2D({self.x}, {self.y})"
+
+    def __str__(self) -> str:
+        """ Returns user representatino of a vector 2d """
+        return f"({self.x}, {self.y})"
+
+    def __iter__(self) -> Iterable:
+        return (i for i in (self.x, self.y))
+
+    def __abs__(self) -> float:
+        return sqrt(self.x ** 2 + self.y **2)
