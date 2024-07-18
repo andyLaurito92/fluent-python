3ecc90cd89d1a28ab1b24158708fbd756a2a4475
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-07-18 10:12:13 +0200

    Adding more special methods to vector2d

--- chapter11-pythonic-object/tests/test_vector2d.py
+++ chapter11-pythonic-object/tests/test_vector2d.py
@@ -4,10 +4,36 @@ from vector2d import Vector2D
 
 class Vector2DTests(TestCase):
     def setUp(self):
-        self.myvector = Vector2D(3, 4)
+        self.myvector: Vector2D = Vector2D(3, 4)
 
     def test_vector_has_attributes_x_and_y(self):
-        self.assertEqual((3, 4), (self.myvector.x, self.myvector.y))
+        self.assertEqual((3.0, 4.0), (self.myvector.x, self.myvector.y))
 
     def test_repr_returns_valid_constructor(self):
-        self.assertEqual("Vector2D(3, 4)", repr(self.myvector))
+        self.assertEqual("Vector2D(3.0, 4.0)", repr(self.myvector))
+
+    def test_str_returns_user_representation(self):
+        self.assertEqual("(3.0, 4.0)", str(self.myvector))
+
+    def test_vector_can_be_unpacked(self):
+        #This behaviour can be achieved by implementing __iter__
+        x, y = self.myvector
+        self.assertEqual(x, 3.0)
+        self.assertEqual(y, 4.0)
+
+    def test_abs_returns_absolute_value(self):
+        self.assertEqual(abs(self.myvector), 5)
+
+    def test_equality_works_at_coordinate_level(self):
+        self.assertEqual(Vector2D(3.0, 4), self.myvector)
+
+    def test_vector_equality_fails_with_whatever_is_not_a_vector(self):
+        # For now, expected behaviour
+        # This hapens bc how we built method __eq__ 
+        self.assertNotEqual(self.myvector, [3, 4])
+
+    def test_nonempty_vector_evaluates_to_true(self):
+        self.assertEqual(self.myvector, True)
+
+    def test_empty_vector_evaluates_to_false(self):
+        self.assertEqual(Vector2D(0, 0), False)
--- chapter11-pythonic-object/vector2d.py
+++ chapter11-pythonic-object/vector2d.py
@@ -1,21 +1,36 @@
-from math import sqrt
+import math
 from typing import Iterable
 
 class Vector2D:
     def __init__(self, x: float, y: float) -> None:
-        self.x = x
-        self.y = y
+        self.x = float(x)
+        self.y = float(y)
 
     def __repr__(self) -> str:
         """ Returns programmer representation of vector 2d """
-        return f"Vector2D({self.x}, {self.y})"
+        class_name = type(self).__name__
+        return f"{class_name}({self.x}, {self.y})"
 
     def __str__(self) -> str:
         """ Returns user representatino of a vector 2d """
-        return f"({self.x}, {self.y})"
+        return str(tuple(self))
 
     def __iter__(self) -> Iterable:
         return (i for i in (self.x, self.y))
 
     def __abs__(self) -> float:
-        return sqrt(self.x ** 2 + self.y **2)
+        return math.hypot(self.x, self.y)
+
+    def __bool__(self) -> bool:
+        # Truthy nomenclature in python
+        return bool(abs(self))
+
+    def __eq__(self, other) -> bool:
+        if type(other) == bool:
+            # Case where we want to evaluate truthy of this elem
+            return bool(self) == other
+        elif type(self) != type(other):
+            return False
+        # This works bc tuple receives an iterator, therefore
+        # these tuples are built upon coordinates in vectors
+        return tuple(self) == tuple(other)
