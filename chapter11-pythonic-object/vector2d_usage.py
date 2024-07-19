c1daab14ed0b03fd46e15811d2fbfcce243ad066
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-07-19 15:40:53 +0200

    Adding private/protected comments

--- chapter11-pythonic-object/vector2d_usage.py
+++ chapter11-pythonic-object/vector2d_usage.py
@@ -17,3 +17,34 @@ def matching_vector(avector: Vector2D) -> None:
 matching_vector(Vector2D(3.0, 4.0))
 matching_vector(Vector2D(0, 3))
 matching_vector(Vector2D(5, 3))
+
+
+"""
+See name mangling in vector2d by showing internal dict
+"""
+
+myvec = Vector2D(2.3, 4.8)
+
+"""
+See in the following example how the class name is appended to
+the so called "private variables".
+
+Remember that there are no private variables in Pytho, as there are
+in Java. What Python provides is this name mangling that allows
+variables to not clash in object inheritance
+"""
+print(myvec.__dict__)
+
+
+"""
+So the reason why I cannot access variable myvec.__x is not
+because it's private
+"""
+# Comment line to see error
+#myvec.__x
+
+"""
+But because it was defined with another name by Python
+"""
+
+myvec._Vector2D__x
