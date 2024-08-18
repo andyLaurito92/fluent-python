d4ec4f91ccb12fe2a0e71d48378cc90a1f4790a5
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-08-18 12:16:55 +0200

    Adding comment

--- chapter13-interface-protocols-abc/static-protocols/tests/test_simplepicker.py
+++ chapter13-interface-protocols-abc/static-protocols/tests/test_simplepicker.py
@@ -3,6 +3,11 @@ from unittest import TestCase
 from simplepicker import SimplePicker
 from randompick import RandomPicker
 
+
+"""
+Instead of using inheritance and enforncing subclassing,
+we just use protocols to enforce the interface
+"""
 class TestSimplePicker(TestCase):
     def is_a_picker(self):
         self.assertTrue(issubclass(SimplePicker, RandomPicker))
