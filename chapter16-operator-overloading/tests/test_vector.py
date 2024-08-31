d97be599c8551e46f9df7f60751bf1b1589e77ee
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-08-31 18:17:49 +0200

    Adding exception: Cannot call invert operator in vector

--- chapter16-operator-overloading/tests/test_vector.py
+++ chapter16-operator-overloading/tests/test_vector.py
@@ -5,10 +5,16 @@ class VectorTests(unittest.TestCase):
     def setUp(self):
         self.vector = Vector(range(1, 4))
 
-
     def test_can_negate_vector(self):
         expected = Vector([-1, -2, -3])
         self.assertEqual(-self.vector, expected)
 
     def test_can_call_pos_unitary_operator(self):
         self.assertEqual(self.vector, Vector(x for x in range(1, 4)))
+
+    def test_cannot_call_invert_operator_in_vector(self):
+        """ We don't want to implement the bit logican negator
+        because it doesn't make sense for our vector"""
+        with self.assertRaises(TypeError) as context:
+            ~self.vector
+            self.assertTrue("bad operand type for unary ~: 'Vector'." in context.exception)
