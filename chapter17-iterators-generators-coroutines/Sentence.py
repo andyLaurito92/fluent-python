cc98b0f2a358ace2e48bc057279ea888ec67a833
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-09-05 10:48:51 +0200

    Adding references to Cpython

--- chapter17-iterators-generators-coroutines/Sentence.py
+++ chapter17-iterators-generators-coroutines/Sentence.py
@@ -31,13 +31,16 @@ The iter built-in function does this:
 iterator
 
 2. If __iter__ is not implemented, but __getitem__ is, then iter() creates an
-iterator that tries to fetch items by index, starting from 0
+iterator that tries to fetch items by index, starting from 0 (This is defined
+in cpython here: https://github.com/python/cpython/blob/b1930bf75f276cd7ca08c4455298128d89adf7d1/Lib/_collections_abc.py#L1032)
 
 3. If that fails, Python raises TypeError, usually saying: "C object is not
 iterable", where C is the class of the target object
 
 Because all python sequences implement __getitem__, they all end up being iterable
 because of item 2.
+
+The iter built in function is defined in Cpython here: https://github.com/python/cpython/blob/main/Python/bltinmodule.c#L1667
 """
 
 class Spam:
