a4af552f148bc439aee0a4470aec3a27a090acd2
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-09-08 11:15:14 +0200

    Adding mapping generators

--- chapter17-iterators-generators-coroutines/playground.py
+++ chapter17-iterators-generators-coroutines/playground.py
@@ -104,3 +104,72 @@ Consumes two iterables in parallel; yields items from it
 whenever the corresponding item in selector_it is truthy
 """
 list(itertools.compress('AbraCadabra', (0, 1, 1, 0, 1, 1, 1, 0, 0)))
+
+
+
+"""
+Mapping generator functions
+"""
+
+sample = [1, 2, 3, 0, 4, 1, 2, 8, 5, 3, 9 , -2, 9, 3]
+
+"""
+accummulate(it, [func]) -> yields accumulated sums; if func is provided,
+yields the result of applying it to the first pair of items, then to the
+second, and so on
+"""
+
+list(itertools.accumulate(sample))
+
+list(itertools.accumulate(sample, min))
+
+"""
+So we can get the min of a list in python by doing this
+"""
+
+for min in itertools.accumulate(sample, min):
+    print("Min so far ", min)
+
+
+"""
+Calculating a product of all elements in a list would be
+"""
+from operator import mul
+
+list(itertools.accumulate(sample, mul))
+
+names = ["Andres", "Gabriela", "Amelia", "Jorge"]
+
+for idx, val in enumerate(names, start=3):
+    print(idx, val)
+
+
+"""
+map returns a generator. As argument, it can take N iterables and
+the function must take the same amount of iterables provided by argument
+"""
+
+lastnames = ["Lastname1", "Lastname2", "Lastname3"]
+
+for full_name in map(operator.add, names, lastnames):
+    print(full_name)
+
+
+"""
+Repeat each letter in the word according to its place in it
+"""
+list(itertools.starmap(operator.mul, enumerate('andres', 1)))
+# 'a' * 1, 'n' * 2, 'd' * 3
+
+"""
+Concatenating 2 lists of strings 
+"""
+list(itertools.starmap(operator.add, zip(names, lastnames)))
+
+"""
+Difference between starmap and map: Map provides a unique argument to
+the function, while starmap is equal of doing func(*args) where *args
+represent an object that can be destructured
+"""
+# This doesn't work
+# list(map(operator.mul, enumerate('andres', 1)))
