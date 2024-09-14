1535f831fadde543f8e895dc4d2d92f085542b1c
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-09-14 12:44:00 +0200

    Adapting logs to resemble original

--- chapter17-iterators-generators-coroutines/extra-classic-coroutines/simulation.py
+++ chapter17-iterators-generators-coroutines/extra-classic-coroutines/simulation.py
@@ -19,28 +19,27 @@ class State(Enum):
     FULL = 2
 
 class Taxi:
-    def __init__(self, id: int, num_travels_to_perform: int) -> NoReturn:
+    def __init__(self, id: int, num_travels_to_perform: int) -> None:
         self.id = id
         self.num_travels_to_perform = num_travels_to_perform
         self.state = State.LEAVE_GARAGE
 
     def simulate(self) -> Generator[State, None, None]:
-        self._log("Leaving garage")
+        #self._log("Leaving garage")
         yield self.state
         while True:
-            self._log("Picking up passanger")
+            #self._log("Picking up passanger")
             yield self._pickup_passenger()
-            self._log("Dropping off passanger")
+            #self._log("Dropping off passanger")
             yield self._drop_passanger()
 
-    def is_in_service(self) -> bool:
         return self.num_travels_to_perform != 0
 
     def _pickup_passenger(self) -> State: 
         self.state = State.FULL
         return self.state
 
-    def _drop_passanger(self) -> NoReturn:
+    def _drop_passanger(self) -> State:
         self.state = State.EMPTY
         self.num_travels_to_perform -= 1
 
@@ -53,26 +52,30 @@ class Taxi:
         self._log("Finishing service")
         raise TaxiFinishedService()
 
-    def _log(self, msg: str) -> NoReturn:
+    def _log(self, msg: str) -> None:
         print(" " * self.id * 3, f"Taxi with id {self.id}: ", msg)
         
 
 class TaxiSimulation:
-    def __init__(self, num_taxis: int) -> NoReturn:
+    def __init__(self, num_taxis: int) -> None:
         self.taxis = []
         print(f"Creating {num_taxis} taxis")
         for i in range(num_taxis):
-            self.taxis.append(Taxi(i, randint(1, 10)).simulate())
+            taxi = Taxi(i, randint(1, 10))
+            self.taxis.append((taxi, taxi.simulate()))
 
-    def start(self) -> NoReturn:
+    def start(self) -> None:
+        time = 0
         print("Starting simulation")
         while self.taxis:
             try:
                 next_taxi_idx = randint(0, len(self.taxis) - 1)
-                next_taxi = self.taxis[next_taxi_idx]
-                next(next_taxi)
+                time = 1 + time + next_taxi_idx
+                taxi, taxi_sim = self.taxis[next_taxi_idx]
+                taxi_state = next(taxi_sim)
+                print(" " * taxi.id * 3, f"taxi: {taxi.id}, Event(time={time}, remaining_pickups={taxi.num_travels_to_perform}, state='{taxi_state}')")
             except TaxiFinishedService as e:
-                self.taxis.remove(next_taxi)
+                self.taxis.remove(self.taxis[next_taxi_idx])
                 num_taxis_left = len(self.taxis)
                 if num_taxis_left > 0:
                     print(f"Still {num_taxis_left} in the simulation")
