2ab63df7b11ce76eb41bdbdc353369e59c9812e4
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-09-19 16:08:32 +0200

    Adding environment. Changing name to repl

--- chapter18-with-match-else-blocks/scheme_interpreter/mylis.py
+++ chapter18-with-match-else-blocks/scheme_interpreter/mylis.py
@@ -62,7 +62,6 @@ def parse_atom(token: str) -> Atom:
 """
 Eval
 """
-
 def eval(expression: Expression) -> Expression:
     if not isinstance(expression, list):
         return expression
@@ -84,7 +83,67 @@ def eval(expression: Expression) -> Expression:
         case _:
             return expression
     
-def run_interpreter():
+
+"""
+Environment variables
+"""
+class Environment(ChainMap[Symbol, Any]):
+    "A ChainMap that allows changing an item in-place"
+
+    def change(self, key: Symbol, value: Any) -> None:
+        "Find where key is defined and change the value there."
+        for map in self.maps:
+            if key in map:
+                map[key] = value
+                return
+        raise KeyError(key)
+
+
+def standard_env() -> Environment:
+    "An environment with some Scheme standard procedures."
+    env = Environment()
+    env.update(vars(math))
+    env.update({
+        '+': op.add,
+        '-': op.sub,
+        '*': op.mul,
+        '/': op.truediv,
+        '//': op.floordiv,
+        '>': op.gt,
+        '<': op.lt,
+        '>=': op.ge,
+        '<=': op.le,
+        '=': op.eq,
+        'abs': abs,
+        'append': lambda *args: list(chain(*args)),
+        'apply': lambda proc, args: proc(*args),
+        'begin': lambda *x: x[-1],
+        'car': lambda x: x[0],
+        'cdr': lambda x: x[1:],
+        'cons': lambda x, y: [x] + y,
+        #'display': lambda x: print(lispstr(x)),
+        'eq?': op.is_,
+        'equal?': op.eq,
+        'filter': lambda *args: list(filter(*args)), # What about the fn?
+        'length': len,
+        'list': lambda *x: list(x),
+        'list?': lambda x: isinstance(x, list),
+        'map': lambda *args: list(map(args)),
+        'max': max,
+        'min': min,
+        'not': op.not_,
+        'null?': lambda x: x == [],
+        'number?': lambda x: isinstance(x, (int, float)),
+        'procedure?': callable,
+        'round': round,
+        'symbol?': lambda x: isinstance(x, Symbol)
+        })
+    return env
+
+"""
+Repl
+"""
+def repl():
     msg_stop = "Stopping interpreter"
     try:
         print("Starting scheme interpreter")
@@ -99,7 +158,7 @@ def run_interpreter():
 
 if __name__ == '__main__':
     if len(sys.argv) == 1: # Only file received 
-        run_interpreter()
+        repl()
     elif len(sys.argv) == 2: # this file + .lisp containig program to parse
         lisp_file = sys.argv[1]
         print(f"Executing file {lisp_file}")
