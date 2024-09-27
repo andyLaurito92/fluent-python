967aa6384fa9016e085268006476df66e20f4035
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-09-27 11:13:52 +0200

    Adding comment

--- chapter20-concurrent-executors/executor_map.py
+++ chapter20-concurrent-executors/executor_map.py
@@ -32,8 +32,12 @@ def main():
     enumerate calls next(results) which in turn will invoke
     _f.result() on the (internal) _f future representing the
     first call, loiter(0). The result method will block until
-    the future is done, therefore each itertation in this loop
-    will have to wait for the next result to be ready
+    the future is done, therefore each iteration in this loop
+    will have to wait for the next result to be ready. This is
+    how ordered is preserved
+    DOWNSIDE: We are not taking advantage on those threads that
+    have already finished the computation! If we want to do that,
+    we need to use both executor.submit + futures.as_completed
 
     You can check the implementation of map here:
     https://github.com/python/cpython/blob/main/Lib/concurrent/futures/_base.py#L575
