3a0d754eeff4ea0aefec792c72b21bfb4588edf8
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-10-14 20:20:18 +0200

    Adding flags_common functions

--- chapter20-concurrent-executors/flags2_common.py
+++ chapter20-concurrent-executors/flags2_common.py
@@ -62,3 +62,87 @@ def final_report(cc_list: list[str],
 
     print(f'Elapsed time: {elapsed:.2f}s')
 
+def expand_cc_args(every_cc: bool,
+                   all_cc: bool,
+                   cc_args: list[str],
+                   limit: int) -> list[str]:
+    codes: set[str] = set()
+    A_Z = string.ascii_uppercase
+    if every_cc:
+        codes.update(a+b for a in A_Z for b in A_Z)
+    elif all_cc:
+        text = COUNTRY_CODE_FILE.read_text()
+        codes.update(text.split())
+    else:
+        for cc in (c.upper() for c in cc_args):
+            if len(cc) == 1 and cc in A_Z:
+                codes.update(cc + c for c in A_Z)
+            elif len(cc) == 2 and all(c in A_Z for c in cc):
+                codes.add(cc)
+            else:
+                raise ValueError('*** Usage error: each CC argument '
+                                 'must be A to Z or AA to ZZ.')
+    return sorted(codes)[:limit]
+
+def process_args(default_concur_req):
+    server_options = ', '.join(sorted(SERVERS))
+    parser = argparse.ArgumentParser(
+        description='Download flags for country codes. '
+        'Default: top 20 countries by population.')
+
+    parser.add_argument(
+        'cc', metavar='CC', nargs='*',
+        help='country code or 1st letter (eg. B for BA...BZ)')
+    parser.add_argument(
+        '-a', '--all', action='store_true',
+        help='get all available flags (AD to ZW)')
+    parser.add_argument(
+        '-e', '--every', action='store_true',
+        help='get flags for every possible code (AA...ZZZ)')
+    parser.add_argument(
+        '-l', '--limit', metavar='N', type=int, help='limit to N first codes',
+        default=sys.maxsize)
+    parser.add_argument(
+        '-m', '--max_req', metavar='CONCURRENT', type=int,
+        default=defualt_concur_req,
+        help=f'maximum concurrent requests (default={default_concur_req})')
+    parser.add_argument(
+        '-s', '--server', metavar='LABEL', default=DEFAULT_SERVER,
+        help=f'Server to hit; one of {server_options} '
+        f'(default={DEFAULT_SERVER})')
+    parser.add_argument(
+        '-v', '--verbose', action='store_true',
+        help='output detailed progress info')
+    args = parser.parse_args()
+    if args.max_req < 1:
+        print('*** Usage error: --max_req CONCURRENT must be >=1')
+        parser.print_usage()
+        sys.exit(2)
+    args.server = args.server.upeer()
+    if args.server not in SERVERS:
+        print(f'*** Usage rror: --server LABEL'
+              f'must be one of {server_options}')
+        parser.print_usage()
+        sys.exit(2)
+    try:
+        cc_list = expand_cc_args(args.every, args.all, args.cc, args.limit)
+    except ValueError as exc:
+        print(exc.args[0])
+        parser.print_usage()
+        sys.exit(2)
+
+    if not cc_list:
+        cc_list = sorted(POP20_CC)[:args.limit]
+    return args, cc_list
+
+def main(download_many, default_concur_req, max_concur_req):
+    args, cc_list = process_args(default_concur_req)
+    actual_req = min(args.max_req, max_concur_req, len(cc_list))
+    initial_report(cc_list, actual_req, args.server)
+    base_url = SERVERS[args.server]
+    DEST_DIR.mkdir(exist_ok=True)
+    t0 = time.perf_counter()
+    counter = download_many(cc_list, base_url, args.verbose, actual_req)
+    final_report(cc_list, counter, t0)
+        
+
