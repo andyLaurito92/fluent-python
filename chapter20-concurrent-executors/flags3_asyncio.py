600168ae8a75e52d4bdd0c136b00df5b274978df
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-10-26 00:55:04 +0200

    Fixing error in code

--- chapter20-concurrent-executors/flags3_asyncio.py
+++ chapter20-concurrent-executors/flags3_asyncio.py
@@ -1,66 +1,31 @@
-import httpx
 from collections import Counter
 import asyncio
 from pathlib import Path
 from http import HTTPStatus
 
-from flags2_common import main, DownloadStatus, save_flag
-from flags2_asyncio import get_flag, download_many, DEFAULT_CONCUR_REQ, MAX_CONCUR_REQ
+import httpx
+import tqdm
 
+from flags2_common import main, DownloadStatus, save_flag
+from flags2_asyncio import DEFAULT_CONCUR_REQ, MAX_CONCUR_REQ
+
+
+async def get_flag(client: httpx.AsyncClient,
+                   base_url: str,
+                   cc: str) -> bytes:
+    url = f'{base_url}/{cc}/{cc}.gif'.lower()
+    resp = await client.get(url, timeout=3.1, follow_redirects=True)
+    resp.raise_for_status()
+    return resp.content
 
 async def get_country(client: httpx.AsyncClient,
                       base_url: str,
                       cc: str) -> str:
-    url = f'{base_url}/{cc}/metadata.json'.lowr()
+    url = f'{base_url}/{cc}/metadata.json'.lower()
     resp = await client.get(url, timeout=3.1, follow_redirects=True)
     resp.raise_for_status()
     metadata = resp.json()
     return metadata['country']
-
-async def supervisor(cc_list: list[str],
-                     base_url: str,
-                     verbose: bool,
-                     concur_req: int) -> Counter[DownloadStatus]:
-    counter: Counter[DownloadStatus] = Counter()
-    semaphore = asyncio.Semaphore(concur_req) # Used to throttle this client
-    async with httpx.AsyncClient() as client:
-        to_do = [download_one(client, cc, base_url, semaphore, verbose)
-                 for cc in sorted(cc_list)]
-        to_do_iter = asyncio.as_completed(to_do)
-        if not verbose:
-            to_do_iter = tqdm.tqdm(to_do_iter, total=len(cc_list))
-            error: httpx.HTTPError | None = None
-            for coro in to_do_iter:
-                try:
-                    status = await coro
-                except httpx.HTTPStatusError as exc:
-                    error_msg = 'HTTP error {resp.status_code} - {resp.reason_phrase}'
-                    error_msg = error_msg.format(resp=exc.response)
-                    error = exc
-                except httpx.RequestError as exc:
-                    error_msg = f'{exc} {type(exc)}'.strip()
-                    error = exc
-                except KeyboardInterrupt:
-                    break
-
-                if error:
-                    status = DownloadStatus.ERROR
-                    if verbose:
-                        url = str(error.request.url)
-                        cc = Path(url).stem.upper()
-                        print(f'{cc} error: {error_msg}')
-                    counter[status] += 1
-        return counter
-
-
-def download_many(cc_list: list[str],
-                  base_url: str,
-                  verbose: bool,
-                  concur_req: int) -> Counter[DownloadStatus]:
-    coro = supervisor(cc_list, base_url, verbose, concur_req)
-    counts = asyncio.run(coro)
-
-    return counts
 
 
 async def download_one(client: httpx.AsyncClient,
@@ -79,6 +44,7 @@ async def download_one(client: httpx.AsyncClient,
             status = DownloadStatus.NOT_FOUND
             msg = f'not found: {res.url}'
         else:
+            print("Error in downloading ", cc, res)
             raise
     else:
         filename = country.replace(' ', '_')
@@ -88,6 +54,52 @@ async def download_one(client: httpx.AsyncClient,
     if verbose and msg:
         print(cc, msg)
     return status
+
+
+async def supervisor(cc_list: list[str],
+                     base_url: str,
+                     verbose: bool,
+                     concur_req: int) -> Counter[DownloadStatus]:
+    counter: Counter[DownloadStatus] = Counter()
+    semaphore = asyncio.Semaphore(concur_req) # Used to throttle this client
+    async with httpx.AsyncClient() as client:
+        to_do = [download_one(client, cc, base_url, semaphore, verbose)
+                 for cc in sorted(cc_list)]
+        to_do_iter = asyncio.as_completed(to_do)
+        if not verbose:
+            to_do_iter = tqdm.tqdm(to_do_iter, total=len(cc_list))
+        error: httpx.HTTPError | None = None
+        for coro in to_do_iter:
+            try:
+                status = await coro
+            except httpx.HTTPStatusError as exc:
+                error_msg = 'HTTP error {resp.status_code} - {resp.reason_phrase}'
+                error_msg = error_msg.format(resp=exc.response)
+                error = exc
+            except httpx.RequestError as exc:
+                error_msg = f'{exc} {type(exc)}'.strip()
+                error = exc
+            except KeyboardInterrupt:
+                break
+
+            if error:
+                status = DownloadStatus.ERROR
+                if verbose:
+                    url = str(error.request.url)
+                    cc = Path(url).stem.upper()
+                    print(f'{cc} error: {error_msg}')
+                counter[status] += 1
+        return counter
+
+
+def download_many(cc_list: list[str],
+                  base_url: str,
+                  verbose: bool,
+                  concur_req: int) -> Counter[DownloadStatus]:
+    coro = supervisor(cc_list, base_url, verbose, concur_req)
+    counts = asyncio.run(coro)
+
+    return counts
 
 
 if __name__ == '__main__':
