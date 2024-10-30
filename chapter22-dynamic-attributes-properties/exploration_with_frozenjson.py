bbd41688369eea7009fd20708db5d7f0d82a2dec
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-10-30 17:28:20 +0100

    Adding comment

--- chapter22-dynamic-attributes-properties/exploration_with_frozenjson.py
+++ chapter22-dynamic-attributes-properties/exploration_with_frozenjson.py
@@ -1,6 +1,12 @@
 from frozenjson import FrozenJSON
 from data_loader import data
 
+
+"""
+Using FrozenJSON class which leverages __getattr__ method, we can access
+json attributes as keys instead of having to access them as dictionary keys
+"""
+
 frozenjson = FrozenJSON(data)
 
 frozenjson.Schedule.events[40].name
