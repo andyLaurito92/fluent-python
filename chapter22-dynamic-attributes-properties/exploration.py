06b9df0c84af8fafbba4356e6518ee2938cfb397
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-10-30 16:32:04 +0100

    Playing around w/the data

---
+++ chapter22-dynamic-attributes-properties/exploration.py
@@ -0,0 +1,14 @@
+import json
+import os
+from pathlib import Path
+
+DATA_FILE = Path(os.path.abspath(os.path.dirname(__file__))) / 'data/osconfeed.json'
+
+with open(str(DATA_FILE)) as fp:
+    data = json.load(fp)
+
+
+sorted(data['Schedule'].keys())
+
+for key, value in sorted(data['Schedule'].items()):
+    print(f'{len(value):3} {key}')
