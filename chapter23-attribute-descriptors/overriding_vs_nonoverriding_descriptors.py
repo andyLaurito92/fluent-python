f7398dea87bb52540e7d9444a14313261285d434
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-11-09 15:55:50 +0100

    Overriding vs nonoverriding descriptors

---
+++ chapter23-attribute-descriptors/overriding_vs_nonoverriding_descriptors.py
@@ -0,0 +1,49 @@
+def cls_name(obj_or_cls):
+    cls = type(obj_or_cls)
+    if cls is type:
+        cls = obj_or_cls
+    return cls.__name__.split('.')[-1]
+
+
+def display(obj):
+    cls = type(obj)
+    if cls is type:
+        return f'<class {obj.__name__}>'
+    elif cls in (type(None), int):
+        return repr(obj)
+    else:
+        return f'<{cls_name(object)} object>'
+
+def print_args(name, *args):
+    pseudo_args = ', '.join(display(x) for x in args)
+    print(f'-> {cls_name(args[0])}.__{name}__({pseudo_args})')
+
+
+class Overriding:
+    """ Data descriptor """
+    def __get__(self, instance, owner):
+        print_args('get', self, instance, owner)
+
+    def __set__(self, instance, value):
+        print_args('set', self, instance, value)
+
+
+class OverridingNoGet:
+    """ Data descriptor without get"""
+    def __set__(self, instance, value):
+        print_args('set', self, instance, value)
+
+
+class NonOverriding:
+    """ non-data descriptor """
+    def __get__(self, instance, owner):
+        print_args('get', self, instance, owner)
+
+
+class Managed:
+    over = Overriding()
+    over_no_get = OverridingNoGet()
+    non_over = NonOverriding()
+
+    def spam(self):
+        print(f'-> Managed.span({display(self)})')
