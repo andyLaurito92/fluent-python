462395284ea15a446a97ae40018e498f42c7f75e
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-11-10 01:34:50 +0100

    Adding examples and more info

--- chapter23-attribute-descriptors/methods_are_descriptors.py
+++ chapter23-attribute-descriptors/methods_are_descriptors.py
@@ -26,6 +26,14 @@ print(type(mytext.reverse), type(Text.reverse))
 # and bound the function to an instance object!
 bounded_method = Text.reverse.__get__(mytext, None)
 
+
+# We can call function reverse from Text class by calling it
+# with an instance of Text
+print(Text.reverse(Text("mounstrito")))
+
+# We can use Text.reverse as a normal function!
+print(list(map(Text.reverse, ("hellohowareyou", ["ho", "ha", "hi"], "dunder"))))
+
 # Now we have a bounded method, not a function anymore
 print(bounded_method)
 
@@ -38,3 +46,20 @@ print(bounded_method == mytext.reverse)
 print(bounded_method == Text.reverse)
 
 print(type(bounded_method.__self__), bounded_method.__self__)
+
+# You can access the underlying function of a bounded method by
+# accessing __func__
+print(bounded_method.__func__)
+
+print(bounded_method.__func__ is Text.reverse)
+
+print(bounded_method.__call__)
+
+
+"""
+The bound method object also has a __call__ method, which handles
+the actual invocation. This method calls the original function
+referenced in __func__, passing the __self__ attribute of the
+method as the first argument. That's how the implicit binding
+of the conventional self argument works
+"""
