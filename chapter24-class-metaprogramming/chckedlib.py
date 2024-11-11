c958ffbb2edfee654ccdb34ead4edd43d1bd4f34
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-11-11 20:31:32 +0100

    Defining remaining methods of checked class

--- chapter24-class-metaprogramming/chckedlib.py
+++ chapter24-class-metaprogramming/chckedlib.py
@@ -45,3 +45,30 @@ class Checked:
             setattr(self, name, value)
         if kwargs:
             self.__flag_unkown_attrs(*kwargs)
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        if name in self._fields():
+            cls = self.__class__
+            descriptor = getattr(cls, name)
+            descriptor.__set__(self, value)
+        else:
+            self.__flag_Unkown_attrs(name)
+
+    def __flag_unkown_attrs(self, *names: str) -> NoReturn:
+        plural = 's' if len(names) > 1 else ''
+        extra = ', '.join(f'{name!r}' for name in names)
+        cls_name = repr(self.__class__.__name__)
+        raise AttributeError(f'{cls_name} object has no attribute{plural} {extra}')
+
+    def _asdict(self) -> dict[str, Any]:
+        return {
+            name: getattr(self, name)
+            for name, attr in self.__class__.__dict__.items()
+            if isinstance(attr, Field)
+        }
+
+    def __repr__(self) -> str:
+        kwargs = ', '.join(
+            f'{key}={value!r}' for key, value in self._asdict().items()
+            )
+        return f'{self.__class__.__name__}({kwargs})'
