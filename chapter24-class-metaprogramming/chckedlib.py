90d38cf7f62f95409113d76dcebbab6f2265b890
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-11-11 20:16:37 +0100

    Adding checkedlib class and a snippet code

---
+++ chapter24-class-metaprogramming/chckedlib.py
@@ -0,0 +1,47 @@
+from collections.abc import Callable
+from typing import Any, NoReturn, get_type_hints
+
+class Field:
+    """ Descriptor that dynamically checks the type of the field """
+    def __init__(self, name: str, constructor: Callable) -> None:
+        if not callable(constructor) or constructor is type(None):
+            raise TypeError(f'{name!r} type hint must be callable')
+        self.name = name
+        self.constructor = constructor
+
+    def __set__(self, instance: Any, value: Any) -> None:
+        if value is ...:
+            value = self.constructor()
+        else:
+            try:
+                value = self.constructor(value)
+            except (TypeError, ValueError) as e:
+                type_name = self.constructor.__name__
+                msg = f'{value!r} is not compatible with {self.name}:{type_name}'
+                raise TypeError(msg) from e
+        instance.__dict__[self.name] = value
+
+
+class Checked:
+    @classmethod
+    def _fields(cls) -> dict[str, type]:
+        return get_type_hints(cls)
+
+    # Documentation of this special method can be found here
+    # https://docs.python.org/3/reference/datamodel.html#object.__init_subclass__
+    def __init_sublclass__(subclass) -> None:
+        """ Method call when a subclass of this class is defined.
+        It gets that new subclass as its first argument"""
+        super().__init_subclass__()
+        for name, constructor in subclass._fields().items():
+            setattr(subclass, name, Field(name, constructor))
+
+    def __init__(self, **kwargs: Any) -> None:
+        for name in self._fields():
+            # Return ... if no value (instead of None)
+            # With this, we make a difference between "no value"
+            # and "user set the value to None"
+            value = kwargs.pop(name, ...) 
+            setattr(self, name, value)
+        if kwargs:
+            self.__flag_unkown_attrs(*kwargs)
---
+++ chapter24-class-metaprogramming/using_checkedlib.py
@@ -0,0 +1,7 @@
+"""
+Before getting into how we use the checked class, small note:
+
+Python defines default values for all its basic types
+"""
+
+print(int(), bool(), str(), float(), dict())
