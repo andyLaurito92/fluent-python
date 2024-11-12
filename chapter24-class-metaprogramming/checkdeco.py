d5f10ce8b0c732ed0f843b29deeb99191d34c369
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-11-12 12:12:44 +0100

    Creating class decorator for checking attr types

--- chapter24-class-metaprogramming/checkdeco.py
+++ chapter24-class-metaprogramming/checkdeco.py
@@ -1,4 +1,42 @@
+from typing import get_type_hints
 """
 TODO: Implement a class decorators that acts as
 class define din checkdeco.py
 """
+
+class TypedField:
+    def __init__(self, name, constructor):
+        self.name = name
+        self.constructor = constructor
+
+    def __set__(self, instance, value):
+        if value is ...:
+            value = self.constructor()
+        elif not isinstance(value, self.constructor):
+            raise ValueError(f"{value} is not of type {self.constructor.__name__!r}")
+        value = self.constructor(value)
+        instance.__dict__[self.name] = value
+
+def checked(theclass):
+    fields = get_type_hints(theclass)
+
+    for field, typeclss in fields.items():
+        typedfield = TypedField(field, typeclss)
+        setattr(theclass, field, typedfield)
+            
+    return theclass
+
+
+@checked
+class Student:
+    name: str
+    age: int
+    studentid: str
+
+andy = Student()
+
+andy.name = "Andy"
+try:
+    andy.name = 3.2
+except ValueError as e:
+    print(e)
