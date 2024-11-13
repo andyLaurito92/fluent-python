7b050e83597260c88b8d58b2f0d87abdcb1b82b4
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-11-13 12:49:56 +0100

    Adding metaclasses examples

---
+++ chapter24-class-metaprogramming/metaclasses.py
@@ -0,0 +1,42 @@
+class MetaBunch(type):
+    def __new__(meta_cls, cls_name, bases, cls_dict):
+        print('ProgrammingLanguage.__new__', meta_cls, cls_name, bases, cls_dict)
+        print(type(meta_cls))
+
+        defaults = {}
+
+        def __init__(self, **kwargs):
+            for name, default in defaults.items():
+                setattr(self, name, kwargs.pop(name, default))
+            if kwargs:
+                extra = ', '.join(kwargs)
+                raise AttributeError(f'No slots left for: {extra!r}')
+
+        def __repr__(self):
+            rep = ', '.join(f'{name}={value!r}'
+                            for name, default in defaults.items()
+                            if (value := getattr(self, name)) != default)
+            return f'{cls_name}({rep})'
+
+        new_dict = dict(__slots__=[], __init__=__init__, __repr__=__repr__)
+
+        for name, value in cls_dict.items():
+            if name.startswith('__') and name.endswith('__'):
+                if name in new_dict:
+                    raise AttributeError(f"Can't set {name!r} in {cls_name!r}")
+                new_dict[name] = value
+            else:
+                new_dict['__slots__'].append(name)
+                defaults[name] = value
+        res = super().__new__(meta_cls, cls_name, bases, new_dict)
+        print(res)
+        return res
+
+
+class Bunch(metaclass=MetaBunch):
+    pass
+    
+class Point(Bunch):
+    x = 0.0
+    y = 0.0
+    color = 'gray'
---
+++ chapter24-class-metaprogramming/myexample_metaclasses.py
@@ -0,0 +1,51 @@
+"""
+By default, python classes are instances of type
+"""
+
+print(type(str))
+
+print(type(int))
+
+class Test:
+    pass
+
+print(type(Test))
+
+print(type(type)) # type is instance of itself :)
+
+print(type(object))
+
+
+class ProgrammingLanguage(type):
+    def __new__(meta_cls, cls_name, bases, cls_dict):
+        print('ProgrammingLanguage.__new__', meta_cls, cls_name, bases, cls_dict)
+        res = super().__new__(meta_cls, cls_name, bases, cls_dict)
+        print('ProgrammingLanguage class created ', res)
+        return res
+
+
+
+class Lisp(metaclass=ProgrammingLanguage):
+    def __init__(self, name):
+        self.name = name
+
+class Scheme(Lisp):
+    name = 'Scheme'
+
+    def __init__(self, num_users):
+        super().__init__(self.name)
+        self.num_users = num_users
+
+
+myscheme = Scheme(10)
+
+print(myscheme)
+
+"""
+To process class Scheme, Python calls
+
+ProgrammingLanguage.__new__(ProgrammingLanguage, 'Scheme', (Lisp,), cls_dict)
+
+When you implement ProgrammingLanguage.__new__, you can manipulate the parameters
+before calling super().__new__ which will eventually end up calling type.__new__
+"""
